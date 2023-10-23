955d6ea53dbf65b5a6b786e03e38abfd8c5c4c06
IrfanM66 <civilwar776677@gmail.com>
2023-10-23 12:50:28 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Permohonan from "./pages/Permohonan/Permohonan";
-import Login from "./pages/Login/Login";
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
         {/* <Route path="/" element={<Login />} /> */}
         <Route path="/landing-page" element={<LandingPage />} />
         <Route path="/permohonan" element={<Permohonan />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/LandingPage/LandingPage";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
